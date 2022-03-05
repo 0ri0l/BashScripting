93e92180bef33c27ffce42bcfdbec820c6ce8d5e
Oriol <oriolt@gmail.com>
2022-03-05 21:50:55 +0100

    Update us_del_awk.sh

--- 0. Comandes útils/us_del_awk.sh
+++ 0. Comandes útils/us_del_awk.sh
@@ -11,9 +11,12 @@
 # Cada línia es compara amb cada patró en ordre; 
 # per a cada patró que concordi amb la línia 
 # s'efectua l'acció corresponent.
-
+echo "Qui és root?
 awk -F: '$1=="root" {print}' /etc/passwd
 # Pregunta pel primer camp de cada línea de /etc/passwd
 # si és root
 # i la imprimeix considerant com a separador 
 # de camps el ":" amb -F: .
+
+echo "Quins són els usuaris del sistema?"
+cat /etc/passwd | awk -F ":" '{print $1}'
