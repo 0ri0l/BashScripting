eb37062dc830ec77f3d6722ab8d93fe22e5c32bc
Oriol <oriolt@gmail.com>
2022-03-07 09:58:15 +0100

    Update echo_read.sh

--- 2. Comanda echo/echo_read.sh
+++ 2. Comanda echo/echo_read.sh
@@ -2,3 +2,5 @@
 echo -e "Siusplau, introdueixi 3 noms d’arxiu:”
 read  arxiu_a arxiu_b arxiu_c  
 echo “Aquests són els arxius escollits :$arxiu_a  $arxiu_b  $arxiu_c”
+echo -e "Escull una opció: \n1-Autodestrucció \n2-Esborrat massiu \n3-Aniquil·lació"
+read elecc
