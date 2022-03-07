986c16b39c17eb2b57e3e6bc22aa9ca83ee80379
Oriol <oriolt@gmail.com>
2022-03-08 00:42:38 +0100

    Create exemple_complert_ordre_if.sh

---
+++ 7. Estructures lògiques de Shell/exemple_complert_ordre_if.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [  "$1"  ==  "dilluns"  ] 
+	then
+		echo "L'argument introduit és Dilluns."
+	elif [ "$1" == “dimarts" ]
+ 	 then 
+ 		echo "L'argument introduit és Dimarts"
+	 else
+ 		echo "L'argument introduit no és ni Dilluns ni Dimarts"
+ 	fi
