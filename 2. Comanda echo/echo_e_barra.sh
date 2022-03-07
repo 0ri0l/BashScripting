a17a2489d4bf5f75e568a40a8ee5cb29d73e3111
Oriol <oriolt@gmail.com>
2022-03-07 09:03:35 +0100

    Create echo_e_barra.sh

---
+++ 2. Comanda echo/echo_e_barra.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Utilizant -e més "\n", s'afegeix un salt de línia
+echo -e "Avui \ntampoc \nserà \nel \nsemestre \ndel \ntribut \na \nla \nmacedònia \ncom \na \npostre"
+# En utilitzar -e més "\t", obtindrem espais de tabulació horitzontal.
+echo -e "\n\tAvui \n\ttampoc \n\tserà \n\tel \n\tdia \n\ten \n\tel \n\tque \n\tLinux\n\ttriomfa \n\tmés \n\tque \n\tla \n\CocaCola"
+# Per obtenir espais de tabulació verticals utilitzarem -e més «\v»
+echo -e "\vAvui \vtampoc \vserà \vel \vmes \vdel \vtribut \va \vCarmen \vde \vMairena"  
