84116f017edde6e969f3fc43b6da9bd5a06f4157
Oriol <oriolt@gmail.com>
2022-03-07 17:08:36 +0100

    Update llistes.sh

--- 4. Variables/llistes.sh
+++ 4. Variables/llistes.sh
@@ -3,9 +3,12 @@ u=(SBD 082 SBDLL321)
 Y[2]=HUSKY
 a=(0 8 2)
 
+# Com afecten els espais?
 echo $u
 echo $Y[2]
 echo $Y[*]
 
-echo $a{a[*]}
+# Volem veure la llista d'elements
+echo ${a[*]}
+# Volem veure el primer element de la llista
 echo ${a[0]}
