11530ce9d019aaa7ef3df4a23b48ddf37dd87f0b
Oriol <oriolt@gmail.com>
2022-03-07 12:58:42 +0100

    Create Exemples.sh

---
+++ 3. Variables globals/Exemples.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# El shell actual
+echo $SHELL
+# El nom complet del directori d'inici de sessió
+echo $SHELL
+# La ruta de cerca d'ordres
+echo $PATH
+# El nom d'inici de sessió
+echo $USER
+#El tipus de terminal
+echo $TERM
+# El directori de treball actual
+echo $PWD
