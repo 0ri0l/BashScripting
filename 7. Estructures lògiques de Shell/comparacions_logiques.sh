7b8ab244a4b18cd06ff7836d26a1fecdad8e04b2
Oriol <oriolt@gmail.com>
2022-03-08 12:34:09 +0100

    Create comparacions_logiques.sh

---
+++ 7. Estructures lògiques de Shell/comparacions_logiques.sh
@@ -0,0 +1,10 @@
+	#!/bin/bash
+	if [ "$#" -ne 2 ] then
+      echo $0 "necessita dos paràmetres"
+      echo "Estàs introduint $# paràmetres.
+else
+		  param1=$1
+      param2=$2
+fi
+echo $param1
+echo $param2
