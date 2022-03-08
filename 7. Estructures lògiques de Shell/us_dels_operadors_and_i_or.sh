2f4788e8000b2e1531d875d876d6c4e57b92b4fc
Oriol <oriolt@gmail.com>
2022-03-08 02:07:56 +0100

    Update us_dels_operadors_and_i_or.sh

--- 7. Estructures lògiques de Shell/us_dels_operadors_and_i_or.sh
+++ 7. Estructures lògiques de Shell/us_dels_operadors_and_i_or.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if  date | grep “div”  &&  `date +’%H’` -gt 17  
+if  date | grep "div"  &&  `date +'%H'` -gt 17  
 then
 	echo "És divendres, és hora de passar-ho bé!!!"
 fi
 
-if [ “$a” –lt 0 –o “$a” –gt 100 ]     
+if [ "$a" –lt 0 –o "$a" –gt 100 ]     
 # RECORDEU que s'ha de deixar espai al voltant del ] i [
 then
-	echo “Límits excedits” 
+	echo "Límits excedits" 
 fi
