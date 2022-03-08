ff65477f8cd5cffe5cee9601a2cf9f3311e11e23
Oriol <oriolt@gmail.com>
2022-03-08 13:02:13 +0100

    Update us_comparacions_logiques.sh

--- 7. Estructures lògiques de Shell/us_comparacions_logiques.sh
+++ 7. Estructures lògiques de Shell/us_comparacions_logiques.sh
@@ -10,3 +10,18 @@ else
 fi
 echo $param1
 echo $param2
+
+	# Ara demanem un número per teclat
+		echo –e "Introdueix un número:\c"
+		# llegim què s'ha introduit per teclat
+    		read numero
+		if [ “$numero” -lt 0 ] 
+		then
+	      	echo "negatiu"
+		else if [ “$number” -eq 0 ] 
+			then
+	      		echo "El número introduït és zero"
+			else
+		       echo "El número introduït és positiu"
+		fi
+fi
