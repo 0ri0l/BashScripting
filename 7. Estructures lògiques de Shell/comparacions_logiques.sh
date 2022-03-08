71a606fc81e4e9a7b77bae435d040f8ec362bb35
Oriol <oriolt@gmail.com>
2022-03-08 12:36:57 +0100

    Update comparacions_logiques.sh

--- 7. Estructures lògiques de Shell/comparacions_logiques.sh
+++ 7. Estructures lògiques de Shell/comparacions_logiques.sh
@@ -1,10 +1,10 @@
 	#!/bin/bash
 	if [ "$#" -ne 2 ] then
-      echo $0 "necessita dos paràmetres"
-      echo "Estàs introduint $# paràmetres.
+      echo $0 necessita dos paràmetres
+      echo Estàs introduint $# paràmetres.
 else
-		  param1=$1
-      param2=$2
+	param1=$1
+	param2=$2
 fi
 echo $param1
 echo $param2
