63c93c973f77aa2e4cc672684f4a64420d0d1bca
Oriol <oriolt@gmail.com>
2022-03-17 11:04:49 +0100

    Update us_if_i_us_cut.sh

--- 8. Condicionals/us_if_i_us_cut.sh
+++ 8. Condicionals/us_if_i_us_cut.sh
@@ -7,8 +7,8 @@ echo 	# aquest 'echo' és per introduir un retorn de línia
 if [ $NUMERO -lt 100 ]; then
 	echo "Ho sento, has introduit un número més petit que 100"
 else
-	PRIMERA_CIFRA=$(echo $NUMERO | cut -c 1)
-	TERCERA_CIFRA=$(echo $NUMERO | cut -c 3)
+	PRIMERA_XIFRA=$(echo $NUMERO | cut -c 1)
+	TERCERA_XIFRA=$(echo $NUMERO | cut -c 3)
 	if [ $PRIMERA_XIFRA = $TERCERA_XIFRA ]; then
 		echo "El número $NUMERO es cap-i-cua"
 	else
