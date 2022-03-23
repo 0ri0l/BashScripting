363efb629e69c78ab6f3a53c6a439f3e71cdf0af
Oriol <oriolt@gmail.com>
2022-03-23 13:14:24 +0100

    Update Exemple_esborrar_logs.sh

--- H_Exemples_complementaris/Exemple_esborrar_logs.sh
+++ H_Exemples_complementaris/Exemple_esborrar_logs.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 #Variables
 LOG_DIR=/var/log
-ROOT_UID=0 # solo lo pueden ejecutar el root
-LINES=50 # Líneas por defecto.
-E_XCD=86 # NO me puedo cambiar de directorio
+ROOT_UID=0 # solament el pot executar el Root
+LINES=50 # Línies per defecte.
+E_XCD=86 # NO em puc canviar de directori
 E_NOTROOT=87 # Salida de No-root error.
-if [ "$UID" -ne "$ROOT_UID" ] # Soy root?
+if [ "$UID" -ne "$ROOT_UID" ] # Sóc Root?
 then
-echo "Debe ser root. Lo siento."
+echo "Ho sento. Has de ser Root."
 exit $E_NOTROOT
 fi
-if [ -n "$1" ] # Número de líneas a preservar?
+if [ -n "$1" ] # Número de línies a preservar?
 then
 lines=$1
 else
-lines=$LINES # valor por defecto.
+lines=$LINES # Valor por defecte.
 fi
 cd $LOG_DIR
 if [ `pwd` != "$LOG_DIR" ]
-# también puede ser if [ "$PWD" != "$LOG_DIR" ]
+# també podria ser if [ "$PWD" != "$LOG_DIR" ]
 then
-echo "No puedo ir a $LOG_DIR."
+echo "No puc anar a $LOG_DIR."
 exit $E_XCD
 fi #
-# Otra forma de hacerlo sería :
+# Una altra forma de fer-ho seria la següent:
 # cd /var/log || {
-# echo "No puedo !" >&2
+# echo "No puc !" >&2
 # exit $E_XCD;
 # }
 tail -n $lines messages > mesg.temp # Salvo en temporal
-mv mesg.temp messages # Muevo.
-cat /dev/null > wtmp #Borro wtmp.
+mv mesg.temp messages # Moc.
+cat /dev/null > wtmp # Esborro wtmp.
 
-echo "Logs Borrados."
+echo "Logs esborratss."
 exit 0
-# Un cero indica que todo Ok.
+# Un zero indica que tot està OK.
