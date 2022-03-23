74a0656d4ae4f2598cd7f0610b645f64a8c7f0c8
Oriol <oriolt@gmail.com>
2022-03-23 13:16:45 +0100

    Update Exemple_esborrar_logs.sh

--- H_Exemples_complementaris/Exemple_esborrar_logs.sh
+++ H_Exemples_complementaris/Exemple_esborrar_logs.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-#Variables
+
+# Mitjançant aquest script esborrarem els logs (esborra /var/log/wtmp i es queda amb 
+# 50 línies – o les que passi l'usuari per línia de comandes – de /var/log/messages)
+
 LOG_DIR=/var/log
 ROOT_UID=0 # solament el pot executar el Root
 LINES=50 # Línies per defecte.
 E_XCD=86 # NO em puc canviar de directori
 E_NOTROOT=87 # Salida de No-root error.
+
 if [ "$UID" -ne "$ROOT_UID" ] # Sóc Root?
 then
 echo "Ho sento. Has de ser Root."
