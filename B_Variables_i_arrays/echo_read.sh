902a2f3dd26324db67f6c1a458e27402629707b6
Oriol <oriolt@gmail.com>
2022-03-24 10:33:35 +0100

    Create echo_read.sh

---
+++ B_Variables_i_arrays/echo_read.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo -e "Siusplau, introdueixi 3 noms d’arxiu:”
+read  arxiu_a arxiu_b arxiu_c  
+echo “Aquests són els arxius escollits :$arxiu_a  $arxiu_b  $arxiu_c”
+echo -e "Escull una opció: \n1) Autodestrucció \n2) Esborrat massiu \n3) Aniquil·lació \n4) Marxa militar"
+read elecc
+echo "Has escollit $elecc"
