3ac6ae0116f291d201c7db1a876c74bfb0a3e0ac
Oriol <oriolt@gmail.com>
2022-03-24 10:41:59 +0100

    Create us_if_us_cut_parametres.sh

---
+++ C_Estructures_condicionals/us_if_us_cut_parametres.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Passarem un número de 3 xifres com a paràmetre
+# i comprovarem si és cap-i-cua o no
+clear
+NUMERO=$1
+if [ $# -ne 1 ]; then
+	echo "No has introduït el número de paràmetre correcte"
+else
+	if [ $NUMERO -lt 100 ] || [ $NUMERO -gt 999 ]; then
+		echo "Ho sento, no has introduït un número de tres xifres"
+	else
+		PRIMERA_XIFRA=$(echo $NUMERO | cut -c 1)
+		TERCERA_XIFRA=$(echo $NUMERO | cut -c 3)
+		if [ $PRIMERA_XIFRA = $TERCERA_XIFRA ]; then
+			echo "El número $NUMERO és cap-i-cua"
+		else
+			echo "El número $NUMERO no és cap-i-cua"
+		fi
+	fi
+fi
