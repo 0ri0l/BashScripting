fce43f48518d17d246c3952b557c714715710ed6
Oriol <oriolt@gmail.com>
2022-03-24 17:37:14 +0100

    Delete B_Variables_i_arrays directory

--- B_Variables_i_arrays/00_us_del_echo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-  echo " Avui és:  " `date`
-  echo "Amb qui estic parlant?" 
-  # Guardo valor introduït per teclat
-  read nom
-  echo "Hola $nom" 
-# Què passaria si substitueixo 
-# les " per ' en l’últim echo
-  
