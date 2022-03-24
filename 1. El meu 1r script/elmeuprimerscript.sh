1c30e1953a942a3202f5307a32444c45bb94593f
Oriol <oriolt@gmail.com>
2022-03-24 09:33:31 +0100

    Delete 1. El meu 1r script directory

--- 1. El meu 1r script/elmeuprimerscript.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-# El primer exemple d’un bash script
-directori=`pwd`
-echo "Hola món!"
-# ATENCIÓ perquè poso un echo sense les (dobles) cometes
-echo La data d’avui és `date`
-echo El directori actual és $directori
