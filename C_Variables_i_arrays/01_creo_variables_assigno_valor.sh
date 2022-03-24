cf396a99462285dedbd66d2c00a04d0bcd23f001
Oriol <oriolt@gmail.com>
2022-03-24 17:38:03 +0100

    Add files via upload

---
+++ C_Variables_i_arrays/01_creo_variables_assigno_valor.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+PREU_COPES=60
+BEVEDORS=10
+A_PAGAR=$[$PREU_COPES / $BEVEDORS]
+echo Cada bevedor ha de pagar $A_PAGAR euros.
---
+++ C_Variables_i_arrays/03_entrecometes.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+# ATENCIÓ amb l’entrecometes que 
+# utilitzo en cada cas
+directori=`pwd`
+echo “Hola món!”
+echo La data d’avui és `date`
+echo El directori actual és $directori
