362dce46c58d0dbaec239f5caafd59023729d433
Oriol <oriolt@gmail.com>
2022-03-25 12:20:53 +0100

    Create if_comparacions_igualtat_diffs.sh

---
+++ D_Estructures_condicionals/if_comparacions_igualtat_diffs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+T1="daredevil"
+T2="batman"
+if [ "$T1" = "$T2" ]; then
+echo expressió avaluada com a verdadera
+else
+echo expressió avaluada com a falsa
+fi
+if [ "$T1" != "$T2" ]; then
+echo expressió avaluada com a verdadera
+else
+echo expressió avaluada com a falsa
+fi
