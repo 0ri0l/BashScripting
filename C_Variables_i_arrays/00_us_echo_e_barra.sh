61b659679da19bbd1d02330008392cb2830b2d87
Oriol <oriolt@gmail.com>
2022-03-28 09:56:23 +0200

    Update 00_us_echo_e_barra.sh

--- C_Variables_i_arrays/00_us_echo_e_barra.sh
+++ C_Variables_i_arrays/00_us_echo_e_barra.sh
@@ -2,6 +2,6 @@
 #Utilizant -e més "\n", s'afegeix un salt de línia
 echo -e "Avui \ntampoc \nserà \nel \nsemestre \ndel \ntribut \na \nla \nmacedònia \ncom \na \npostre"
 # En utilitzar -e més "\t", obtindrem espais de tabulació horitzontal.
-echo -e "\n\tAvui \n\ttampoc \n\tserà \n\tel \n\tdia \n\ten \n\tel \n\tque \n\tLinux\n\ttriomfa \n\tmés \n\tque \n\tla \n\CocaCola"
+echo -e "\n\tAvui \n\ttampoc \n\tserà \n\tel \n\tdia \n\ten \n\tel \n\tque \n\tLinux\n\ttriomfa \n\tmés \n\tque \n\tla \n\tCocaCola"
 # Per obtenir espais de tabulació verticals utilitzarem -e més «\v»
 echo -e "\vAvui \vtampoc \vserà \vel \vmes \vdel \vtribut \va \vCarmen \vde \vMairena" 
