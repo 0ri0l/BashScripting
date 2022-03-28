f40e3d1a7e1192c0a3f9bf71a831f92d05a79ae5
Oriol <oriolt@gmail.com>
2022-03-28 17:37:10 +0200

    Update and rename 00_us_echo_e_barra.sh to 00_us_echo.sh

--- C_Variables_i_arrays/00_us_echo.sh
+++ C_Variables_i_arrays/00_us_echo.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-#Utilizant -e més "\n", s'afegeix un salt de línia
+
+# Aquí tens l'oportunitat de conèixer
+# les diferències entre entre les cometes dèbils 
+# i les fortes
+
+echo " Avui és:  " `date`
+echo "Amb qui estic parlant?" 
+# Guardo valor introduït per teclat
+read nom
+echo "Hola $nom" 
+# Què passaria si substitueixo 
+# les " per ' en l’últim echo
+
+# Ara traurem profit del paràmetre -e per l'echo
+# Per exemple, amb "\n", s'afegeix un salt de línia
 echo -e "Avui \ntampoc \nserà \nel \nsemestre \ndel \ntribut \na \nla \nmacedònia \ncom \na \npostre"
 # En utilitzar -e més "\t", obtindrem espais de tabulació horitzontal.
 echo -e "\n\tAvui \n\ttampoc \n\tserà \n\tel \n\tdia \n\ten \n\tel \n\tque \n\tLinux\n\ttriomfa \n\tmés \n\tque \n\tla \n\tCocaCola"
