676b11d3a7707d58181b67c215804641d992c058
Oriol <oriolt@gmail.com>
2022-03-30 12:53:16 +0200

    Create 02_variables_concatenar_valors(II).sh

---
+++ C_Variables_i_arrays/02_variables_concatenar_valors(II).sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Defineixo una variable amb el resultat
+# d'executar una comanda
+# CONCATENO valors
+quisocjo=$(which $SHELL)
+
+echo "El tipus de terminal amb el que treballo és $quisocjo"
