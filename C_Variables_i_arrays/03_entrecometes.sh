19a26dbf64f3956f982d8c435470967f79490278
Oriol <oriolt@gmail.com>
2022-03-30 11:11:38 +0200

    Update 03_entrecometes.sh

--- C_Variables_i_arrays/03_entrecometes.sh
+++ C_Variables_i_arrays/03_entrecometes.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 directori=`pwd`
-echo “Hola món!”
+echo "Hola món!"
 # Si una cadena està tancada amb cometes dobles (febles),
 # les referències a les variables ($variables) són substituïdes
 # pels seus valors.
