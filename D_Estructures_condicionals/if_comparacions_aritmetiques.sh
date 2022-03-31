1e6df9657b127f9fe1f61af5be166326592f1fe0
Oriol <oriolt@gmail.com>
2022-04-01 01:01:50 +0200

    Update if_comparacions_aritmetiques.sh

--- D_Estructures_condicionals/if_comparacions_aritmetiques.sh
+++ D_Estructures_condicionals/if_comparacions_aritmetiques.sh
@@ -6,10 +6,10 @@ read -p "Introdueix l'alçada de la persona nº 1 : " ALTURA1
 read -p "Introdueix l'alçada de la persona nº 2 : " ALTURA2
 read -p "Introdueix l'alçada de la persona nº 3 : " ALTURA3
 
-if [ $ALTURA1 -gt $ALTURA2 ] && [ $ALTURA1 -gt $ALTURA3 ]; then
+if [ $ALT1 -gt $ALT2 ] && [ $ALT1 -gt $ALT3 ]; then
 	echo "La persona nº 1 és la més alta "
-elif [ $ALTURA2 -gt $ALTURA3 ]; then
-	echo "La persona nº 2 és la més alta ($ALTURA2 centímetres)"
+elif [ $ALT2 -gt $ALT3 ]; then
+	echo "La persona nº 2 és la més alta ($ALT2 centímetres)"
 else
-	echo "La persona nº 3 és la més alta ($ALTURA1 centímetres)"
+	echo "La persona nº 3 és la més alta ($ALT1 centímetres)"
 fi
