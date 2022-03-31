71a74b4a600f59868320dc0a8b4c719d3646ea73
Oriol <oriolt@gmail.com>
2022-03-31 11:34:48 +0200

    Update if_comparacions_igualtat_diffs.sh

--- D_Estructures_condicionals/if_comparacions_igualtat_diffs.sh
+++ D_Estructures_condicionals/if_comparacions_igualtat_diffs.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 T1="daredevil"
 T2="batman"
+
+# Com que utilitzo cometes flebles (")
+# haig d'utilitzar un claudàtor simple
 if [ "$T1" = "$T2" ]; then
 echo expressió avaluada com a verdadera
 else
 echo expressió avaluada com a falsa
 fi
+
+# Utilitzo doble claudàtor per 
+# per (de)mostrar que no calen les "
 if [[ $T1 != $T2 ]]; then
 echo expressió avaluada com a verdadera
 else
 echo expressió avaluada com a falsa
+fi
+
+echo "introdueix un valor:"
+read valor
+# Ara faig una comparació amb una crida a una
+# variable dins d'una altra crida
+if [[ $(($valor % 2)) == 0 ]]
+then 
+echo és un valor parell
+else
+echo és un valor imparell
 fi
