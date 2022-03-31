6687bf227d42c34c1d16e95e8e3f7b2dea13b737
Oriol <oriolt@gmail.com>
2022-04-01 01:16:02 +0200

    Update if_amb_ops_d_aval_d_arxius.sh

--- D_Estructures_condicionals/if_amb_ops_d_aval_d_arxius.sh
+++ D_Estructures_condicionals/if_amb_ops_d_aval_d_arxius.sh
@@ -2,9 +2,9 @@
 # Amb aquest script analitzarem l'alçada de tres persones
 # i direm quina és la més alta
 clear
-read -p "Introdueix l'alçada de la persona nº 1 : " ALTURA1
-read -p "Introdueix l'alçada de la persona nº 2 : " ALTURA2
-read -p "Introdueix l'alçada de la persona nº 3 : " ALTURA3
+read -p "Introdueix l'alçada de la persona nº 1 : " ALT1
+read -p "Introdueix l'alçada de la persona nº 2 : " ALT2
+read -p "Introdueix l'alçada de la persona nº 3 : " ALT3
 
 if [ $ALT1 -gt $ALT2 ] && [ $ALT1 -gt $ALT3 ]; then
 	echo "La persona nº 1 és la més alta "
