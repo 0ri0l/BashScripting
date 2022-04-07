a078396d5ba46ee39c4454c2317330d38bf1a4bb
Oriol <oriolt@gmail.com>
2022-04-07 19:04:39 +0200

    Update llegir_un_arxiu_linia_x_linia_amb_while.sh

--- E_Bucles/llegir_un_arxiu_linia_x_linia_amb_while.sh
+++ E_Bucles/llegir_un_arxiu_linia_x_linia_amb_while.sh
@@ -2,8 +2,7 @@
 # Un truc important en els scripts és llegir 
 # un arxiu línia a línia per poder processar-lo 
 # i aplicar canvis en ell.
-cat /etc/passwd | while read LINE
+cat /etc/passwd | while read LINIA
 do
-    echo "Vols modificar $LINIA? (s/n)"
-    read SINO << /dev/tty
+    echo $LINIA
 done
