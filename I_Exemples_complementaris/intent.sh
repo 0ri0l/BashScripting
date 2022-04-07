73abdb151132220235b7e335da79c08c0b62f77c
Oriol <oriolt@gmail.com>
2022-04-07 19:11:21 +0200

    Update intent.sh

--- I_Exemples_complementaris/intent.sh
+++ I_Exemples_complementaris/intent.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 exec 3>&1 # fem un arreglillo creant un nou stream (3). Si no, no podem visualitzar el menú correctament
-retorn=$(dialog --calendar "hello..." 5 40 30 3 2016 2>&1 1>&3)
+retorn=$(dialog --calendar "hello..." 5 45 24 8 2022 2>&1 1>&3)
 exec 3>&- # retornem els streams com estaven (cancelem el stream 3)
 if [ $? -eq 1 ]; then
     echo "Has cancelat!"
