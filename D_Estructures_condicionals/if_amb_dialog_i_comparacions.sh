484f483071a40ede7bc302610fe4db44a0e57e7f
Oriol <oriolt@gmail.com>
2022-04-07 19:43:41 +0200

    Update if_amb_dialog_i_comparacions.sh

--- D_Estructures_condicionals/if_amb_dialog_i_comparacions.sh
+++ D_Estructures_condicionals/if_amb_dialog_i_comparacions.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-# Amb aquest script analitzarem l'alçada de tres persones
-# i direm quina és la més alta
 
-# Per utilitzar el dialog
-# hauràs de instal·lar-lo
+# Amb aquest script analitzarem l'alçada
+# de 3 persones i direm quina és la més alta
 
-dialog --title "T'has de decidir.." --yesno "Estàs preparat?" 0 0; clear
-resposta=$?
-  if [ $resposta -eq 0 ]
-  then
-	clear
-	read -p "Introdueix l'alçada de la persona nº 1 : " ALT1
-	read -p "Introdueix l'alçada de la persona nº 2 : " ALT2
-	read -p "Introdueix l'alçada de la persona nº 3 : " ALT3
+clear
+	# Una de les limitacions de Bash és que no pot gestionar 
+	# números de coma flotant o dobles.
+	# Hem de introduir valors enterns
+	read -p "Introdueix l'alçada (en cm) de la persona nº 1 : " ALT1
+	read -p "Introdueix l'alçada (en cm) de la persona nº 2 : " ALT2
+	read -p "Introdueix l'alçada (en cm) de la persona nº 3 : " ALT3
 
 	if [ $ALT1 -gt $ALT2 ] && [ $ALT1 -gt $ALT3 ]; then
 		echo "La persona nº 1 és la més alta "
@@ -21,6 +18,3 @@ resposta=$?
 		else
 		echo "La persona nº 3 és la més alta ($ALT3 centímetres)"
 	fi
-  else
-    exit 1
-fi
