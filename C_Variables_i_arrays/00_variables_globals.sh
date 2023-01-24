84e500e724464b27ec90525c51f738f4bb8940de
Oriol <oriolt@gmail.com>
2023-01-24 09:32:03 +0100

    Create 00_variables_globals.sh

---
+++ C_Variables_i_arrays/00_variables_globals.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Mostrem el shell actual
+echo $SHELL
+# Identifico la pantalla 
+echo $DISPLAY
+# Mostro el nom complert del directori d'inici de sessió
+echo $HOME
+# Mostro la ruta de cerca d'ordres
+echo $PATH
+# Mostro la ruta de cerca per a pàgines <man>
+echo $MANPATH
+# mostro les cadenes d'indicació primàries i secundàries
+# Mmmm.... De què serveiexen les cadenes d'indicació primàries i secundàries?
+echo $PS1 
+echo $PS2
+# Mostro el nom d'inici de sessió
+echo $USER
+#Mostro el tipus de terminal
+echo $TERM
+# Mostro el directori de treball actual
+echo $PWD
