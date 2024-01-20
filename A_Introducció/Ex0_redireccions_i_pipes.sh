52d5be918b1625d7b23cca5c94ab1ed35bf5bd23
Oriol <oriolt@gmail.com>
2024-01-20 20:10:04 +0100

    Update Ex0_redireccions_i_pipes.sh

--- A_Introducció/Ex0_redireccions_i_pipes.sh
+++ A_Introducció/Ex0_redireccions_i_pipes.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-ls -la > Hola_Mon
+arxiu= "Hola_mon.txt"
+
+ls -la > $arxiu
 
 echo "ATENCIÓ perquè llistem amb 'ls' i veiem"
 echo "que hi ha l'arxiu Hola_Mon"
@@ -8,10 +10,11 @@ ls
 
 echo "Ara mostro el contingut"
 echo "de l'arxiu Hola_Mon"
-cat Hola_Mon
+cat $arxiu
 
 echo "Quantes línies té el (nou) arxiu Hola_Mon?"
-cat Hola_Mon | wc -l
+cat $arxiu | wc -l
 
 echo "AFEGIM el valor a l'arxiu Hola_Mon"
-cat Hola_Mon | wc -l >> Hola_Mon
+cat $arxiu | wc -l >> $arxiu
+# Hem afegit contingut al final de l'arxiu (si l'arxiu no hagués existit, s'hauria creat)
