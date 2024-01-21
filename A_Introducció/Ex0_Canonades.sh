62e108f99c9d95216673c375e3fc8cf31d269129
Oriol <oriolt@gmail.com>
2024-01-21 22:48:46 +0100

    Update Ex0_Canonades.sh

--- A_Introducció/Ex0_Canonades.sh
+++ A_Introducció/Ex0_Canonades.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#!/bin/bash
-
 # En aquest Script definim una canonada on, amb l'ordre "top" i "grep", farem una cerca dins d'un fitxer 
 # de les línies que coincideixen amb un patró determinat.
 
