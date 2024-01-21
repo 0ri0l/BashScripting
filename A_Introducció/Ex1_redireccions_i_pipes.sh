235832cb4c2998203f2ca5ff3980b19dbe042f5e
Oriol <oriolt@gmail.com>
2024-01-21 11:32:19 +0100

    Update Ex1_redireccions_i_pipes.sh

--- A_Introducció/Ex1_redireccions_i_pipes.sh
+++ A_Introducció/Ex1_redireccions_i_pipes.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
 
-arxiu="Hola_mon"
+arxiu="Adeu_mon"
 
 ls -la > $arxiu
-
-echo "ATENCIÓ perquè llistem amb 'ls' i veiem què hi ha a l'arxiu"
-ls
-
-echo "Ara mostro el contingut de l'arxiu Hola_Mon"
-cat $arxiu
-
-echo "Quantes línies té el (nou) arxiu Hola_Mon?"
-cat $arxiu | wc -l
-
-echo "AFEGIM el valor a l'arxiu Hola_Mon"
-cat $arxiu | wc -l >> $arxiu
-# Hem afegit contingut al final de l'arxiu (si l'arxiu no hagués existit, s'hauria creat)
-
-# -------- ESTAN PASANDO COSAS!! -------- 
-
+ 
 # Ara llegeixo (i mostro) l'arxiu línia a línia amb el bucle While i l'operand <
 while read -r line; do
   echo "$line"
 done <$arxiu
+
