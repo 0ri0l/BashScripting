40a1b09b0eb5f1be73327746a275d4e8ca26fe25
Oriol <oriolt@gmail.com>
2024-01-21 10:52:48 +0100

    Update Ex1_redireccions_i_pipes.sh

--- A_Introducció/Ex1_redireccions_i_pipes.sh
+++ A_Introducció/Ex1_redireccions_i_pipes.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+arxiu="Adeu_mon"
+
+echo "ATENCIÓ! Faig un llistat del que hi ha al directori"
+ls -l
+
+# Ara utilitzo l'ordre while per executar un bucle mentre fem la lectura
+while read -r line; do
+  echo "$line"
+done <$arxiu
