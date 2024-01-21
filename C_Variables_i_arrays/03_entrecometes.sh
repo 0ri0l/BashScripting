bf10010547d6e02915fe2f7e4afedfb4b77143e3
Oriol <oriolt@gmail.com>
2024-01-22 00:21:16 +0100

    Update 03_entrecometes.sh

--- C_Variables_i_arrays/03_entrecometes.sh
+++ C_Variables_i_arrays/03_entrecometes.sh
@@ -3,11 +3,13 @@
 directori=`pwd`
 nom=Oriol
 echo "Hola món!"
+
 # Si una cadena està tancada amb cometes dobles (febles),
 # les referències a les variables ($variables) són substituïdes
 # pels seus valors.
 
 echo La data d’avui és `date`
+
 # Una cadena tancada amb cometes obertes 
 # es tracta com una ordre i el shell intenta executar-la. 
 echo $nom, el directori actual és $directori
