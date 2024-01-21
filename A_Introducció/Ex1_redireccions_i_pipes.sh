238b9d5621b383baf9e488b8b3e8499871e07eb5
Oriol <oriolt@gmail.com>
2024-01-21 11:42:15 +0100

    Update Ex1_redireccions_i_pipes.sh

--- A_Introducció/Ex1_redireccions_i_pipes.sh
+++ A_Introducció/Ex1_redireccions_i_pipes.sh
@@ -16,9 +16,13 @@ done <$arxiu
 # Per tant, un here-document és un bloc de codi de propòsit especial, que utilitza una forma de redirecció d'E/S 
 # per proporcionar una llista d'ordres a un programa o ordre interactiu com cat, ftp, php, o un editor de text
 
-# Des del terminal del comandes executa la següent línia d'ordres
-cat <<EOF > script.sh
+# Per exemple, ara assignarem una cadena de multiples linies a un arxiu
+# Si l'arxiu no existeix, el crearà i subistituirà el seu contingut. 
+# I, de pas, veurem que fa l'operand <<
+cat<<EOF>script.sh
 #!/bin/bash
 echo \$PWD
 echo $PWD
 EOF
+
+# Si mires el contingut del teu directori, veuràs que hi ha un (nou) arxiu anomenat script.sh
