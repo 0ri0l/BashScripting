6a2d8293773832c0e9d417d370189b80241b70a7
Oriol <oriolt@gmail.com>
2024-01-21 23:09:34 +0100

    Update Ex0_Canonades.sh

--- A_Introducció/Ex0_Canonades.sh
+++ A_Introducció/Ex0_Canonades.sh
@@ -3,6 +3,6 @@
 # En aquest Script definim una canonada on, amb l'ordre "top" i "grep", farem una cerca dins d'un fitxer 
 # de les línies que coincideixen amb un patró determinat.
 
-# Per exemple, farem una cerca del procés "konsole" i desarem les dades resultants en un arxiu.
+# Per exemple, farem una cerca del procés "systemd".
 
-top | grep -e "systemd" >> resultat
+top | grep -e "systemd"
