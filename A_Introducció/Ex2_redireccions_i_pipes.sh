cec91994102a91fa8301b35fe4d00b71b6162423
Oriol <oriolt@gmail.com>
2024-01-21 20:35:56 +0100

    Delete A_Introducció/Ex2_redireccions_i_pipes.sh

--- A_Introducció/Ex2_redireccions_i_pipes.sh
+++
@@ -1,6 +0,0 @@
-# Quan treballem amb scripts, un mètode que ens facilita la creació de scripts és el Here-document (Heredoc).
-# Els Heredoc són un mètode de redireccionament que utilitzem per passar strings - "una quantitat de caràcters" - a un document 
-# o bé, un script que estiguem creant.
-
-# Per tant, un here document és un bloc de codi de propòsit especial, que utilitza una forma de redirecció d'E/S 
-# per proporcionar una llista d'ordres a un programa o ordre interactiu com cat, ftp, php, o un editor de text
