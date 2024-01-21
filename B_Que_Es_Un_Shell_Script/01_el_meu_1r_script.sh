5dfbbe0d40a7bd7d3dd8f476faa03d71aa9fda18
Oriol <oriolt@gmail.com>
2024-01-22 00:23:51 +0100

    Delete B_Que_Es_Un_Shell_Script/01_el_meu_1r_script.sh

--- B_Que_Es_Un_Shell_Script/01_el_meu_1r_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# El primer exemple d’un bash script
-directori=`pwd`
-echo "Hola món!"
-# ATENCIÓ perquè poso un echo sense les (dobles) cometes
-# ALERTA...
-echo La data actual és `date`
-echo El directori actual és $directori
