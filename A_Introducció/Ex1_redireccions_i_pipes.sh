a6b3d5c658f57e199ac3a591ecf285d7bc5da272
Oriol <oriolt@gmail.com>
2024-01-21 11:33:31 +0100

    Update Ex1_redireccions_i_pipes.sh

--- A_Introducció/Ex1_redireccions_i_pipes.sh
+++ A_Introducció/Ex1_redireccions_i_pipes.sh
@@ -9,3 +9,9 @@ while read -r line; do
   echo "$line"
 done <$arxiu
 
+# Quan treballem amb scripts, un mètode que ens facilita la creació de scripts és el Here-document (Heredoc).
+# Els Heredoc són un mètode de redireccionament que utilitzem per passar strings - "una quantitat de caràcters" - 
+# a un document, o bé, a un script que estiguem creant.
+
+# Per tant, un here-document és un bloc de codi de propòsit especial, que utilitza una forma de redirecció d'E/S 
+# per proporcionar una llista d'ordres a un programa o ordre interactiu com cat, ftp, php, o un editor de text
