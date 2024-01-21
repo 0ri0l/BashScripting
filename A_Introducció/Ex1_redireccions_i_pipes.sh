1743e083b3019d5a19df9390b6a500845a008d7a
Oriol <oriolt@gmail.com>
2024-01-21 10:53:35 +0100

    Update Ex1_redireccions_i_pipes.sh

--- A_Introducció/Ex1_redireccions_i_pipes.sh
+++ A_Introducció/Ex1_redireccions_i_pipes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-arxiu="Adeu_mon"
+arxiu2="Adeu_mon"
 
 echo "ATENCIÓ! Faig un llistat del que hi ha al directori"
 ls -l
@@ -8,4 +8,4 @@ ls -l
 # Ara utilitzo l'ordre while per executar un bucle mentre fem la lectura
 while read -r line; do
   echo "$line"
-done <$arxiu
+done <$arxiu2
