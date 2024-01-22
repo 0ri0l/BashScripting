c725b5a096ffebbee211deb7a8b5a52121af82d8
Oriol <oriolt@gmail.com>
2024-01-22 08:18:12 +0100

    Update Ex_Sortides_Estandard.sh

--- A_Introducció/Ex_Sortides_Estandard.sh
+++ A_Introducció/Ex_Sortides_Estandard.sh
@@ -10,10 +10,17 @@ read nom  # stdin
 
 if [ -z "$nom" ]; then
     echo "Error!! NO has introduiit el teu nom." >&2  # stderr
+
 # Verifiquem si el nom està buit i emetem un missatge d'error 
-# en la sortida estàndard (stderr) si és el cas.
+# en la sortida estàndard (stderr), si és el cas.
+# El número 0 representa a stdin, 1 a stdout, i 2 a stderr. 
 
     exit 1
+
+# Exit (code) 0 vol dir Success
+# Exit (code) 1 vol dir General errors, errors de miscel·lània, com per exemple, una divisió per zero i altres operacions errònies
+# Exit (code) 2 es tracta d'un error per males construccions o sintaxis en el shell, per exemple, empty_function() {}
+    
 fi
 
 # >& és la sintaxis per redirigir un flux (stream) a 
