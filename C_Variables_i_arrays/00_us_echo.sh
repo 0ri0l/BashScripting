664403494be66220c3c981c6049004fdca9f7f73
Oriol <oriolt@gmail.com>
2024-01-22 08:28:08 +0100

    Update 00_us_echo.sh

--- C_Variables_i_arrays/00_us_echo.sh
+++ C_Variables_i_arrays/00_us_echo.sh
@@ -14,9 +14,15 @@ echo "Hola $nom"
 
 # Ara traurem profit del paràmetre -e per l'echo
 # Per exemple, amb "\n", s'afegeix un salt de línia
+
 echo -e "Avui \ntampoc \nserà \nel \nsemestre \ndel \ntribut \na \nla \nmacedònia \ncom \na \npostre"
+
 # En utilitzar -e més "\t", obtindrem espais de tabulació horitzontal.
+
 echo -e "\n\tAvui \n\ttampoc \n\tserà \n\tel \n\tdia \n\ten \n\tel \n\tque \n\tLinux\n\ttriomfa \n\tmés \n\tque \n\tla \n\tCocaCola"
+
 # Per obtenir espais de tabulació verticals utilitzarem -e més «\v»
+
 echo -e "\vAvui \vtampoc \vserà \vel \vmes \vdel \vtribut \va \vCarmen \vde \vMairena" 
-# Prova de treure el -e. Què passa?!
+
+# Prova de treure el -e. Què c***ons passa?!
