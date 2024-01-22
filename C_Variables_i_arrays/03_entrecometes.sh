aec0699c05e6321b2e3c9011e180f78f0cf46668
Oriol <oriolt@gmail.com>
2024-01-22 08:26:48 +0100

    Update 03_entrecometes.sh

--- C_Variables_i_arrays/03_entrecometes.sh
+++ C_Variables_i_arrays/03_entrecometes.sh
@@ -4,13 +4,25 @@ directori=`pwd`
 nom=Oriol
 echo "Hola món!"
 
+VARIABLE1=`echo o\`echo riol\``
+VARIABLE2=$(echo o$(echo riol))
+
 # Si una cadena està tancada amb cometes dobles (febles),
 # les referències a les variables ($variables) són substituïdes
 # pels seus valors.
 
+echo "Hola!! Avui és $date"
 echo La data d’avui és `date`
 
 # Una cadena tancada amb cometes obertes 
 # es tracta com una ordre i el shell intenta executar-la. 
 
 echo $nom, el directori actual és $directori
+
+# Amb $() també resulta MOLT senzill niar diverses execucions d'ordres.
+# Amb les cometes, en anar nidant elements, la seva llegibilitat es complica
+# i, per què no dir-ho?, queda més lleig
+
+echo VARIABLE1
+echo --.--.--.--
+echo VARIABLE2
