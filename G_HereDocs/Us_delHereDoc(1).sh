947ede89756bb791954f7e33a257d48fc40964db
Oriol <oriolt@gmail.com>
2024-02-07 08:45:00 +0100

    Create Us_delHereDoc(1).sh

---
+++ G_HereDocs/Us_delHereDoc(1).sh
@@ -0,0 +1,11 @@
+$ cat <<AixoEsUnDelimitador
+Hola sóc jo
+Em preguntava si després de tants anys t'agradaria que ens trobessim
+Per repassar-ho tot
+Diuen que se suposa que el temps t'ha de curar, però jo no he curat gaire
+$ cat <<1
+Hola
+Sóc jo
+1
+Hola
+Sóc
