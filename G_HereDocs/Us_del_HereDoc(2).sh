b311bf1266b971941c19e971edeeabc65cb42c34
Oriol <oriolt@gmail.com>
2024-02-08 11:58:59 +0100

    Update Us_del_HereDoc(2).sh

--- G_HereDocs/Us_del_HereDoc(2).sh
+++ G_HereDocs/Us_del_HereDoc(2).sh
@@ -4,6 +4,8 @@ NOM=$1
 file=$2
 
 cat >> $file << EOF
+# Enlloc de mostrar la sortida per pantalla, podem redireccionar 
+# a un arxiu utilitzant els operadors > i >>
 Hola $NOM
 Aquest és un  bloc de línies
 que afegiré (>>) en l'arxiu que m'has indicat (variable file)
