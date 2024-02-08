8d7038bd266d4a6f15b1ab80abe070cfbaea36fb
Oriol <oriolt@gmail.com>
2024-02-08 11:56:41 +0100

    Update Us_del_HereDoc(1).sh

--- G_HereDocs/Us_del_HereDoc(1).sh
+++ G_HereDocs/Us_del_HereDoc(1).sh
@@ -3,5 +3,8 @@
 NOM='Matthew'
 COGNOM='Murdock'
 cat <<EOF
+# Què passaria si tanquessim el delimitador entre comilles simples o dobles?
+# Si tanquem el delimitador amb comilles dobles, per exemple, 
+# el shell no realizarà l'expansió de paràmetres ni la substitució de comandes.
 Hello $NOM $COGNOM
 EOF
