7fee42c3b7beae8dab64ae321a3f194def312109
Oriol <oriolt@gmail.com>
2024-02-13 12:59:33 +0100

    Create 0_us_basic_del_trap.sh

---
+++ IIts_a_trap/0_us_basic_del_trap.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# Després de la comanda trap hi ha les instruccions/ordres
+#  que s'executaran quan arribi(n) la senyal (o les senyals)
+
+trap "echo Muaaaahhhhh... No em pots matar!!" SIGINT SIGTERM
+echo "Per si de cas... El PID és $$"
+
+# Defineixo un bucle que s'executi infitament, forever and ever
+while true
+do
+        echo "Estic a l'espera de novetats: Sempre pots fer ^C"
+        sleep 2
+done
