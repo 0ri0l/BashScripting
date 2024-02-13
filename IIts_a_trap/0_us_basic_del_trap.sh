a8903a9caf1f48a306422a92f7356de65f59f73a
Oriol <oriolt@gmail.com>
2024-02-13 19:47:24 +0100

    Update 0_us_basic_del_trap.sh

--- IIts_a_trap/0_us_basic_del_trap.sh
+++ IIts_a_trap/0_us_basic_del_trap.sh
@@ -3,7 +3,8 @@
 # Després de la comanda trap hi ha les instruccions/ordres
 #  que s'executaran quan arribi(n) la senyal (o les senyals)
 
-trap "echo Muaaaahhhhh... No em pots matar!!" SIGINT SIGTERM
+trap "echo Muaaaahhhhh... No em pots matar!!" SIGINT SIGTERM SIGTSTP
+
 echo "Per si de cas... El PID és $$"
 
 # Defineixo un bucle que s'executi infitament, forever and ever
